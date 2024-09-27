a24d58abf6c9f87cef8896eee876d058a8b362ad
RodionPlakhotniuk <raidertwk@gmail.com>
2024-09-27 21:12:15 +0300

    Changed the functionality according to the adjustments, namely the calculation functionality, variable names for good readability

--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -4,22 +4,24 @@ public class MainApp {
 
         int orderNumber = 1;
         String product = "smartphone";
-        int day = 5;
-        double salesPerDay = 12153.41 / 5;
-        double totalSales = salesPerDay * day;
-
-
+        int salesDay = 5;
+        double costOneProduct = 220.971090909;
+        int soldUnitsDay = 11;
+        double salesOneDay = soldUnitsDay * costOneProduct;
+        double salesAllDay = salesDay * salesOneDay;
         System.out.printf("Product № %d: %s,\n" +
                "total sales for %d days is EUR %.2f,\n" +
-                "sales by day is EUR %.2f.\n" , orderNumber, product, day, totalSales, salesPerDay);
+                "sales by day is EUR %.2f.\n" , orderNumber, product, salesDay, salesAllDay, salesOneDay);
         orderNumber = 2;
         product = "laptop";
-        day = 7;
-        salesPerDay = 10486.85 / 7;
-        totalSales = salesPerDay * day;
+        salesDay = 7;
+        costOneProduct = 374.53035;
+        soldUnitsDay = 4;
+        salesOneDay = soldUnitsDay * costOneProduct;
+        salesAllDay = salesDay * salesOneDay;
         System.out.printf("Product № %d: %s,\n" +
                 "total sales for %d days is EUR %.2f,\n" +
-                "sales by day is EUR %.2f.\n" , orderNumber, product, day, totalSales, salesPerDay);
+                "sales by day is EUR %.2f.\n" , orderNumber, product, salesDay, salesAllDay, salesOneDay);
 
     }
 }
